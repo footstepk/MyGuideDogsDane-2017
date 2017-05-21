5e5d3b7b720ad94786665ad1cc8149a9997a2bf8
Kok Heng Chan <kok.chan2@mail.dcu.ie>
2017-05-21 10:30:29 +0100

    A class encapsulate the notion of the MapBox utility

---
+++ src/Utils.java
@@ -0,0 +1,62 @@
+package com.ospit.heng.myguidedogsdane;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.drawable.Drawable;
+import android.support.annotation.DrawableRes;
+import android.support.annotation.NonNull;
+import android.support.v4.content.res.ResourcesCompat;
+
+import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.annotations.Icon;
+import com.mapbox.mapboxsdk.annotations.IconFactory;
+
+/**
+  * Implemented by Kok Heng 05/04/2017
+  * realter on 25/04/2017
+  * A call MapBox Utility
+  */
+
+public class Utils {
+
+  /**
+   * <p>
+   * Returns the Mapbox access token set in the app resources.
+   * </p>
+   * It will first search for a token in the Mapbox object. If not found it
+   * will then attempt to load the access token from the
+   * {@code res/values/dev.xml} development file.
+   *
+   * @param context The {@link Context} of the {@link android.app.Activity} or {@link android.app.Fragment}.
+   * @return The Mapbox access token or null if not found.
+   */
+  public static String getMapboxAccessToken(@NonNull Context context) {
+    try {
+      // Read out AndroidManifest
+      String token = Mapbox.getAccessToken();
+      if (token == null || token.isEmpty()) {
+        throw new IllegalArgumentException();
+      }
+      return token;
+    } catch (Exception exception) {
+      // Use fallback on string resource, used for development
+      int tokenResId = context.getResources()
+        .getIdentifier("mapbox_access_token", "string", context.getPackageName());
+      return tokenResId != 0 ? context.getString(tokenResId) : null;
+    }
+  }
+
+  /**
+   * Demonstrates converting any Drawable to an Icon, for use as a marker icon.
+   */
+  public static Icon drawableToIcon(@NonNull Context context, @DrawableRes int id) {
+    Drawable vectorDrawable = ResourcesCompat.getDrawable(context.getResources(), id, context.getTheme());
+    Bitmap bitmap = Bitmap.createBitmap(vectorDrawable.getIntrinsicWidth(),
+      vectorDrawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
+    Canvas canvas = new Canvas(bitmap);
+    vectorDrawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
+    vectorDrawable.draw(canvas);
+    return IconFactory.getInstance(context).fromBitmap(bitmap);
+  }
+}
